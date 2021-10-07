866374fb5850875863cf4559b4b8358ede973731
Taylor Ivanoff <taylorivanoff@gmail.com>
2021-10-07 18:23:58 +1100

    Styling changes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Products') }}
+            Products
         </h2>
     </x-slot>
 
@@ -10,7 +10,7 @@
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
                 @foreach ($products as $product)
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
-                        <a href="{{ route('products.show', $product) }}" class="text-lg font-semibold">
+                        <a href="{{ route('products.show', $product) }}" class="font-semibold">
                             {{ $product->title }}
                         </a>
 
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -9,21 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
                 @if ($orders->count())
-                @foreach ($orders as $order)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
-                    <div class="font-semibold mb-2 border-b-2">Order #{{ $order->id }}</div>
-                    <div class="grid grid-cols-2">
-                        @foreach ($order->products as $product)
-                        <div class="mb-2">
-                            {{ $product->title }}
-                            <br>
-                            <a class="text-indigo-500" href="{{ route('products.downloads.show', $product) }}">Download</a>
+                    @foreach ($orders as $order)
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
+                        <div class="font-semibold mb-2">Order #{{ $order->id }}</div>
+                        <div class="grid grid-cols-2">
+                            @foreach ($order->products as $product)
+                            <div class="mb-2 row">
+                                {{ $product->title }}
+                                <br>
+                                <a class="text-indigo-500" href="{{ route('products.downloads.show', $product) }}">Download</a>
+                            </div>
+                            @endforeach
+                            <div><span class="font-semibold">Total</span><br>@money($order->total())</div>
                         </div>
-                        @endforeach
-                        <div>Total:<br>@money($order->total())</div>
                     </div>
-                </div>
-                @endforeach
+                    @endforeach
                 @else
                     <p>You have no orders.</p>
                 @endif
